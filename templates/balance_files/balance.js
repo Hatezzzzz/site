54ebc9f6529382696980bd1358971657a59f0da0
Hatezzzzz <130417494+Hatezzzzz@users.noreply.github.com>
2023-04-11 15:17:54 +0600

    Delete templates directory

--- templates/balance_files/balance.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $("a.toggled").click(function (e) {
-        e.preventDefault();
-        var target = $(this).data('id');
-        $('div#'+target).toggle(300);
-    });
-});
